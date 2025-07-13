c095206092d7aa9e896f8bfac679531095aa83a2
Maksym Chava <114568029+MaksymChava1337@users.noreply.github.com>
2025-07-13 18:23:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,11 @@
         circle.classList.remove('highlight');
       }, 1200);
     }, 300);
+
+
+    const burger = document.getElementById('burger');
+    const nav = document.getElementById('main-nav');
+
+burger.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
